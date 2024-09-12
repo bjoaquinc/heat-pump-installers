f81ee650ebc997f2271e8bf78467061e8402bc4a
Joaquin Coromina <joaquincoromina@Joaquins-MacBook-Air.local>
2024-09-12 13:14:24 -0400

    Added Favicons and firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,12 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 export default defineNuxtConfig({
+  ssr: true,
   nitro: {
     firebase: {
       gen: 2,
-    }
+      nodeVersion: '20',
+    },
   },
   build: {
     transpile: ["vuetify"],
@@ -17,7 +19,7 @@ export default defineNuxtConfig({
         config.plugins.push(vuetify({autoImport: true}))
       })
     },
-    '@nuxtjs/device'
+    '@nuxtjs/device',
   ],
   vite: {
     vue: {
@@ -28,9 +30,18 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      link: [
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+      ],
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' },
       ],
     },
   },
